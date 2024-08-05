b8acab24b0a9c11767e1564356fe455bdd276eb3
sao1234567 <saodake15@gmail.com>
2024-08-05 16:40:45 +0900

    8/5 prac

---
+++ print/0006までのまとめ.py
@@ -0,0 +1,15 @@
+## printの問題
+# Valorantを出力してください
+print("Valorant")
+# 以下の通りに出力せよ
+# Fortnite
+# Apex
+# Valorant
+print("Fortnite \nApex \nValorant")
+# printを４回使い、以下の通りに出力せよ
+# Apex Fortnite
+# Valorant: Aimlab
+print("Apex" ,end=" ")
+print("Fortnite")
+print("Valorant" ,end=" ")
+print("Aimlab")
